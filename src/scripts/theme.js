41d6320aaccaf406d94a170543aa7b42beaecb51
sarahholden <sarahbeth.holden@gmail.com>
2020-08-22 21:36:30 -0700

    Home'

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -79,139 +79,139 @@ $(document).ready(function() {
   });
 
 
-    // /* ---------------------------------------------
-    //   SPLIT TEXT
-    // ------------------------------------------------ */
-    // var controller = new ScrollMagic.Controller();
-    // var tl = gsap.timeline();
-    //
-    // document.fonts.ready.then(function () {
-    //   var splitText = new SplitText('[data-reveal="lines"]', {type:"lines", linesClass: 'split-line-++'});
-    //   var splitTextMasked = new SplitText('[data-reveal="lines-masked"]', {type:"lines", linesClass: 'split-line-child'});
-    //   var splitTextMaskedInner = new SplitText('[data-reveal="lines-masked"]', {type:"lines", linesClass: 'split-line-++'});
-    //   new SplitText('[data-reveal="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-child'});
-    //   new SplitText('[data-reveal="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-++'});
-    //
-    // $('[data-reveal="lines-masked"]').addClass('loaded');
-    // $('[data-reveal="lines-masked-desc"]').addClass('loaded');
-    //
-    //   // resize function
-    //   $(window).resize(function() {
-    //     tl.progress(1);
-    //     splitText.revert();
-    //     splitTextMasked.revert();
-    //     splitTextMaskedInner.revert();
-    //   });
-    //
-    //   $('.list-animation-wrapper').each(function() {
-    //     var scrollOffset = $(this).data('offset') || 0;
-    //     var $thisWrapper = $(this);
-    //     new ScrollMagic.Scene({triggerElement: this, triggerHook: .9, offset: scrollOffset})
-    //       .setClassToggle(this, "scrolled") // add class toggle
-    //       .reverse(false)
-    //
-    //       .addTo(controller)
-    //       .on("enter", function (e) {
-    //         if ($thisWrapper.find('.animate-item').length > 0) {
-    //           $listItems = $thisWrapper.find('.animate-item');
-    //         } else {
-    //           $listItems = $thisWrapper.find('li');
-    //         }
-    //         $listItems.each(function(i) {
-    //           var delay = i * 100;
-    //           var self = this;
-    //           setTimeout(function() {
-    //             $(self).addClass('fade-in');
-    //           }, delay);
-    //         });
-    //       });
-    //   });
-    //
-    //   $('.js-preload').each(function() {
-    //     var scrollOffset = $(this).data('offset') || 0;
-    //     var $thisWrapper = $(this);
-    //     new ScrollMagic.Scene({triggerElement: this, triggerHook: .9, offset: scrollOffset})
-    //       // .setClassToggle(this, "preload") // add class toggle
-    //       .reverse(false)
-    //       .addTo(controller)
-    //       .on("enter", function (e) {
-    //         $thisWrapper.removeClass('js-preload');
-    //         setTimeout(function () {
-    //           $thisWrapper.removeClass('js-loading');
-    //         }, 1000);
-    //       });
-    //   });
-    //
-    // // END OF FONT LOADING
-    // });
-    //
-    //
-    // /* ---------------------------------------------
-    //   PARALLAX & SCROLLING ZOOM EFFECTS
-    // ------------------------------------------------ */
-    // document.fonts.ready.then(function () {
-    //   $('[data-trigger="scroll"], [data-anim="scroll"]').each(function() {
-    //     var scrollWrapper = this;
-    //     var scrollOffset = $(this).data('offset') || 0;
-    //     new ScrollMagic.Scene(
-    //       {
-    //         triggerElement: this,
-    //         offset: scrollOffset,
-    //         triggerHook: .85
-    //       })
-    //       .setClassToggle(this, "js-animate") // add class toggle
-    //       // .addIndicators('classAdd')
-    //       .reverse(false)
-    //       .addTo(controller);
-    //
-    //
-    //       // SCALE IMAGE UP OR DOWN
-    //       var $imageToScale = $(this).find('[data-anim="scale"]')
-    //       if ($imageToScale.length > 0) {
-    //         var scaleFrom = $imageToScale.data('scale-from') != undefined ? parseFloat($imageToScale.data('scale-from')) : 1;
-    //         var scaleTo = $imageToScale.data('scale-to') != undefined ? parseFloat($imageToScale.data('scale-to')) : 1.09;
-    //         var pointToTrigger = $imageToScale.data('trigger-hook') != undefined ? parseFloat($imageToScale.data('trigger-hook')) : 0.4;
-    //         var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
-    //
-    //         var timelineScale = new TimelineMax();
-    //         var imageToAnimate = $imageToScale;
-    //         timelineScale.fromTo(imageToAnimate, 1, {scale: scaleFrom}, {scale: scaleTo});
-    //
-    //         var scaleScene = new ScrollMagic.Scene({
-    //           triggerElement: scrollWrapper,
-    //           triggerHook: pointToTrigger,
-    //           duration: dataDuration
-    //         })
-    //         .setTween(timelineScale)
-    //         .addTo(controller);
-    //       }
-    //
-    //       if ($(window).innerWidth() > 768) {
-    //         // PARALLAX EFFECT
-    //         var $itemToTranslate = $(this).find('[data-anim="parallax"]');
-    //         if ($itemToTranslate.length > 0) {
-    //           $itemToTranslate.each(function() {
-    //             var timelineParallax = new TimelineMax();
-    //             var translateFrom = $(this).data('translate-from') != undefined ? parseFloat($(this).data('translate-from')) : 40;
-    //             var translateTo = $(this).data('translate-to') != undefined ? parseFloat($(this).data('translate-to')) : -40;
-    //             var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
-    //             timelineParallax.fromTo($(this), 1, {y: translateFrom}, {y: translateTo});
-    //
-    //             var scene = new ScrollMagic.Scene({
-    //               triggerElement: scrollWrapper,
-    //               triggerHook: 0.4,
-    //               duration: dataDuration
-    //             })
-    //             .setTween(timelineParallax)
-    //             .addTo(controller);
-    //           });
-    //         }
-    //       }
-    //
-    //   });
-    //
-    // // END OF FONT LOADING
-    // });
+  /* ---------------------------------------------
+    SPLIT TEXT
+  ------------------------------------------------ */
+  var controller = new ScrollMagic.Controller();
+  var tl = gsap.timeline();
+
+  document.fonts.ready.then(function () {
+    var splitText = new SplitText('[data-reveal="lines"]', {type:"lines", linesClass: 'split-line-++'});
+    var splitTextMasked = new SplitText('[data-reveal="lines-masked"]', {type:"lines", linesClass: 'split-line-child'});
+    var splitTextMaskedInner = new SplitText('[data-reveal="lines-masked"]', {type:"lines", linesClass: 'split-line-++'});
+    new SplitText('[data-reveal="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-child'});
+    new SplitText('[data-reveal="lines-masked-desc"] p', {type:"lines", linesClass: 'split-line-++'});
+
+  $('[data-reveal="lines-masked"]').addClass('loaded');
+  $('[data-reveal="lines-masked-desc"]').addClass('loaded');
+
+    // resize function
+    $(window).resize(function() {
+      tl.progress(1);
+      splitText.revert();
+      splitTextMasked.revert();
+      splitTextMaskedInner.revert();
+    });
+
+    $('.list-animation-wrapper').each(function() {
+      var scrollOffset = $(this).data('offset') || 0;
+      var $thisWrapper = $(this);
+      new ScrollMagic.Scene({triggerElement: this, triggerHook: .9, offset: scrollOffset})
+        .setClassToggle(this, "scrolled") // add class toggle
+        .reverse(false)
+
+        .addTo(controller)
+        .on("enter", function (e) {
+          if ($thisWrapper.find('.animate-item').length > 0) {
+            $listItems = $thisWrapper.find('.animate-item');
+          } else {
+            $listItems = $thisWrapper.find('li');
+          }
+          $listItems.each(function(i) {
+            var delay = i * 100;
+            var self = this;
+            setTimeout(function() {
+              $(self).addClass('fade-in');
+            }, delay);
+          });
+        });
+    });
+
+    $('.js-preload').each(function() {
+      var scrollOffset = $(this).data('offset') || 0;
+      var $thisWrapper = $(this);
+      new ScrollMagic.Scene({triggerElement: this, triggerHook: .9, offset: scrollOffset})
+        // .setClassToggle(this, "preload") // add class toggle
+        .reverse(false)
+        .addTo(controller)
+        .on("enter", function (e) {
+          $thisWrapper.removeClass('js-preload');
+          setTimeout(function () {
+            $thisWrapper.removeClass('js-loading');
+          }, 1000);
+        });
+    });
+
+  // END OF FONT LOADING
+  });
+
+
+  /* ---------------------------------------------
+    PARALLAX & SCROLLING ZOOM EFFECTS
+  ------------------------------------------------ */
+  document.fonts.ready.then(function () {
+    $('[data-trigger="scroll"], [data-anim="scroll"]').each(function() {
+      var scrollWrapper = this;
+      var scrollOffset = $(this).data('offset') || 0;
+      new ScrollMagic.Scene(
+        {
+          triggerElement: this,
+          offset: scrollOffset,
+          triggerHook: .85
+        })
+        .setClassToggle(this, "js-animate") // add class toggle
+        // .addIndicators('classAdd')
+        .reverse(false)
+        .addTo(controller);
+
+
+        // SCALE IMAGE UP OR DOWN
+        var $imageToScale = $(this).find('[data-anim="scale"]')
+        if ($imageToScale.length > 0) {
+          var scaleFrom = $imageToScale.data('scale-from') != undefined ? parseFloat($imageToScale.data('scale-from')) : 1;
+          var scaleTo = $imageToScale.data('scale-to') != undefined ? parseFloat($imageToScale.data('scale-to')) : 1.09;
+          var pointToTrigger = $imageToScale.data('trigger-hook') != undefined ? parseFloat($imageToScale.data('trigger-hook')) : 0.4;
+          var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
+
+          var timelineScale = new TimelineMax();
+          var imageToAnimate = $imageToScale;
+          timelineScale.fromTo(imageToAnimate, 1, {scale: scaleFrom}, {scale: scaleTo});
+
+          var scaleScene = new ScrollMagic.Scene({
+            triggerElement: scrollWrapper,
+            triggerHook: pointToTrigger,
+            duration: dataDuration
+          })
+          .setTween(timelineScale)
+          .addTo(controller);
+        }
+
+        if ($(window).innerWidth() > 768) {
+          // PARALLAX EFFECT
+          var $itemToTranslate = $(this).find('[data-anim="parallax"]');
+          if ($itemToTranslate.length > 0) {
+            $itemToTranslate.each(function() {
+              var timelineParallax = new TimelineMax();
+              var translateFrom = $(this).data('translate-from') != undefined ? parseFloat($(this).data('translate-from')) : 40;
+              var translateTo = $(this).data('translate-to') != undefined ? parseFloat($(this).data('translate-to')) : -40;
+              var dataDuration = $(this).data('duration') != undefined ? $(this).data('duration') : '100%';
+              timelineParallax.fromTo($(this), 1, {y: translateFrom}, {y: translateTo});
+
+              var scene = new ScrollMagic.Scene({
+                triggerElement: scrollWrapper,
+                triggerHook: 0.4,
+                duration: dataDuration
+              })
+              .setTween(timelineParallax)
+              .addTo(controller);
+            });
+          }
+        }
+
+    });
+
+  // END OF FONT LOADING
+  });
 
 
   /* ---------------------------------------------
